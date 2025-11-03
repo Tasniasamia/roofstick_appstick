eeffdef482326aaa49811f3a59e9f41537472d5c
tasniaarhamarham <tasniaarhamarham@gmail.com>
2025-11-03 15:30:50 +0600

    work ongoing

--- components/(site)/home1/hero.tsx
+++ components/(site)/home1/hero.tsx
@@ -1,103 +1,65 @@
-// import Image from "next/image";
-// import React from "react";
-
-// const HeroSection1 = () => {
-//   return (
-//     <div className="relative w-full h-[937px]">
-//       {/* Full width background image */}
-//       <Image
-//         src="/hero.png"
-//         fill
-//         alt="hero image"
-//         className="2xl:object-fill object-cover "
-//         priority
-//       />
-
-//       {/* Content area inside 1840px container */}
-//       <div className="absolute inset-0 mt-[120px] h-[876px]">
-//         <div className="max-w-[1840px] relative h-full mx-auto px-6 text-black z-10">
-//           <div className="absolute bottom-0 h-full w-full">
-// <div className="w-[777px] h-[360px] relative bottom-0">
-//   <h1 className="text-[128px]  font-medium w-fit h-fit text-white m-0 p-0 leading-[95px] font-roboto absolute text-center right-1/2 translate-x-1/2 top-0 z-30">
-//     Buy
-//   </h1>
-
-//   <Image
-//     src="/img1.png"
-//     alt="img1"
-//     width={777}
-//     height={360}
-//     className="max-h-full z-20 absolute min-h-full object-fill"
-//   />
-// </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default HeroSection1;
-
 import Image from "next/image";
 import React from "react";
 
 const HeroSection1 = () => {
   return (
-    <div className="hero-wrapper-parent overflow-hidden">
-      {/* ✅ Background Image */}
+    <>
       <Image
         src="/hero.png"
-        fill
+        width={1000}
+        height={1080}
         alt="hero image"
-        className="lg:object-fill object-cover"
+        className="absolute inset-0 h-[1080px] w-full "
         priority
       />
+      <div className="relative w-full h-[1080px]">
+     
+        <div className="hero-wrapper">
+          <div className="hero-img1">
+            <h1 className="hero-img1-text">
+              Buy
+            </h1>
 
-      {/* ✅ Absolute positioned content container */}
-      <div className="hero-parent">
-        <div className="max-w-[1840px] mx-auto hero-wrapper">
-          {/* Example 1 */}
-          <div className="absolute bottom-[130px] left-0 hero-img ">
             <Image
               src="/img1.png"
               alt="img1"
-              fill
-              className="object-fill rounded-md"
+              width={777}
+              height={360}
+              className="hero-img1-img"
             />
-            <h1 className="text-[128px]  font-medium w-fit h-fit text-white m-0 p-0 leading-[95px] font-roboto absolute text-center right-1/2 translate-x-1/2 top-0 z-30">
-              Buy
-            </h1>
           </div>
-
-          {/* Example 2 */}
-          <div className="  hero-img2 ">
+          {/* <div className="w-[306px] h-[445px] absolute top-[120px] left-[calc(50%+52px)] -translate-x-1/2">
             <Image
               src="/img2.png"
-              alt="img2"
-              fill
-              className="object-fill rounded-md"
+              alt="img1"
+              width={306}
+              height={445}
+              className="w-full h-full z-20 absolute object-cover"
             />
-            <h1 className="absolute top-1/2 -right-10 -rotate-90  -translate-y-1/2 text-[90px] font-medium text-white  font-roboto leading-[0.9] z-30 text-center">
-              Sell
+
+            <h1
+              className="absolute top-1/2 right-[-82px] -translate-y-1/2 
+                  -rotate-90  text-white font-roboto font-medium text-[128px] leading-[95px] tracking-[0] not-italic z-30 text-center m-0 p-0"
+            >
+              Sale
             </h1>
           </div>
-
-          {/* Example 3 */}
-          <div className=" hero-img3 ">
-            <Image
-              src="/img3.png"
-              alt="img3"
-              fill
-              className="object-fill rounded-md"
-            />
+          <div className="w-[653px] h-[365px] absolute bottom-[354px] right-0">
             <h1 className="text-[128px]  font-medium w-fit h-fit text-white m-0 p-0 leading-[95px] font-roboto absolute text-center right-1/2 translate-x-1/2 top-0 z-30">
               Rent
             </h1>
-          </div>
+
+            <Image
+              src="/img3.png"
+              alt="img1"
+              width={653}
+              height={365}
+              className="w-full h-full z-20 absolute object-cover"
+            />
+          </div> */}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
