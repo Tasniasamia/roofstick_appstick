755bcc4ec207a25f050a0917df6a9addeddeb915
tasniaarhamarham <tasniaarhamarham@gmail.com>
2025-11-04 12:27:43 +0600

    title paragraph ready

--- components/(site)/home1/hero.tsx
+++ components/(site)/home1/hero.tsx
@@ -13,12 +13,22 @@ const HeroSection1 = () => {
         priority
       />
       <div className="relative w-full lg:h-[1080px] md:h-[700px] h-full">
-     
         <div className="hero-wrapper">
-          <div className="hero-img1">
-            <h1 className="hero-img1-text">
-              Buy
+          <div className="max-w-[1320px] mx-auto pt-[120px] z-50 relative">
+            <h1 className="text-primary xl:text-[64px] sm:text-[50px] text-[32px] font-raleway xl:leading-[130%] leading-[120%] lg:pt-[50px] 2xl:pt-[120px] font-extrabold sm:text-start text-center">
+              Unlock Your Future <br className="sm:block hidden"/> Home with Roofstick
             </h1>
+            <p className="lg:text-[#717171] md:text-[#05073C] text-[#717171] mt-6 text-base font-roboto xl:w-[491px] lg:w-[380px] sm:w-[400px] sm:text-start text-center lg:font-normal font-medium">
+              Simplify buying, renting, and managing properties with Roofstick
+              your all in one real estate solution
+            </p>
+
+             {/* tab */}
+
+          </div>
+
+          <div className="hero-img1">
+            <h1 className="hero-img1-text font-roboto font-medium">Buy</h1>
 
             <Image
               src="/img1.png"
@@ -37,16 +47,10 @@ const HeroSection1 = () => {
               className="hero-img2-img"
             />
 
-            <h1
-              className="hero-img2-text"
-            >
-              Sale
-            </h1>
+            <h1 className="hero-img2-text font-roboto font-medium">Sale</h1>
           </div>
           <div className="hero-img3">
-            <h1 className="hero-img3-text">
-              Rent
-            </h1>
+            <h1 className="hero-img3-text font-roboto font-medium">Rent</h1>
 
             <Image
               src="/img3.png"
