39f7590ce3b048b6a83ece08c80e4620b984833f
tasniaarhamarham <tasniaarhamarham@gmail.com>
2025-11-05 21:04:25 +0600

    feature property done

--- components/(site)/home/properties.tsx
+++ components/(site)/home/properties.tsx
@@ -1,8 +1,10 @@
+"use client";
 import PropertyCard from "@/components/common/card/propertyCard";
 import HeaderTitle from "@/components/common/headerTitle";
-import React from "react";
+import React, { useState } from "react";
 
 const Properties = () => {
+  const [tab, setTab] = useState("sale");
   return (
     <div className="lg:mt-[120px] mt-[60px]">
       <div className="container">
@@ -13,11 +15,33 @@ const Properties = () => {
           }
           customClass="items-center text-center"
         />
-        <div className="pt-6 grid md:grid-cols-3 sm:grid-cols-2 xl:gap-6 gap-3 sm:place-content-start place-content-center">
-          <PropertyCard />
-          <PropertyCard />
-          <PropertyCard />
+        <div className="flex flex-row justify-center pt-10 pb-6 gap-6 items-center">
+          <button
+            onClick={() => setTab("sale")}
+            className={`flex gap-2 cursor-pointer sm:w-fit w-fit justify-center items-center font-roboto font-medium px-6 py-3 rounded-[10px] text-sm transition-all duration-300 ${
+              tab === "sale"
+                ? "bg-[#4CAC40] text-white shadow-md scale-105 border border-transparent"
+                : "bg-white text-[#4CAC40] hover:bg-gray-50 border border-[#4CAC40]"
+            }`}
+          >
+            <span>For Sale</span>
+          </button>
 
+          <button
+            onClick={() => setTab("buy")}
+            className={`flex gap-2  w-fit cursor-pointer sm:w-fit  justify-center items-center font-roboto font-medium px-6 py-3 rounded-[10px] text-sm transition-all duration-300 ${
+              tab === "buy"
+                ? "bg-[#4CAC40] text-white shadow-md scale-105 border border-transparent"
+                : "bg-white text-[#4CAC40] hover:bg-gray-50 border border-[#4CAC40]"
+            }`}
+          >
+            <span>For Buy</span>
+          </button>
+        </div>
+        <div className="pt-6  grid md:grid-cols-3 sm:grid-cols-2 xl:gap-6 gap-3 sm:place-content-start place-content-center">
+          <PropertyCard />
+          <PropertyCard />
+          <PropertyCard />
         </div>
       </div>
     </div>
--- components/common/card/propertyCard.tsx
+++ components/common/card/propertyCard.tsx
@@ -1,13 +1,18 @@
-import { ArrowRight, ChevronRight } from "lucide-react";
+import {
+  ArrowRight,
+  Bath,
+  Bed,
+  ChevronRight,
+  LocateIcon,
+  SquaresIntersect,
+} from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
 const PropertyCard = () => {
   return (
-    <div className="p-2.5 sm:w-fit w-[300px] h-fit 2xl:custom-shadow2 border border-[#E8EAE8] rounded-xl">
-      <div
-        className="2xl:w-[404px] 2xl:h-[404px]  lg:h-[300px] h-[200px]  w-full relative rounded-xl"
-      >
+    <div className="p-2.5 sm:w-fit w-[300px] h-fit bg-white custom-shadow2  border border-[#E8EAE8] rounded-xl">
+      <div className="2xl:w-[404px] 2xl:h-[404px]  lg:h-[300px] h-[200px]  w-full relative rounded-xl">
         <Image
           src="/img1.png"
           width={500}
@@ -25,8 +30,7 @@ const PropertyCard = () => {
               Read More
             </span>
             <span className="lg:h-8 md:h-5 md:w-5 lg:w-8 w-8 h-8 rounded-full flex items-center justify-center bg-white">
-              <ArrowRight className="lg:text-[20px] text-[10px]! "  />
-
+              <ArrowRight className="lg:text-[20px] text-[10px]! " />
             </span>
           </button>
           <span className="font-roboto lg:text-xl text-sm font-semibold text-white lg:me-3.5 me-1">
@@ -35,8 +39,37 @@ const PropertyCard = () => {
         </div>
       </div>
       <div className="bg-[#EBFFFD] mt-2.5 rounded-xl p-3.5">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-        
+        <h4 className="text-primary font-roboto text-[24px] font-semibold leading-normal line-clamp-1" title="Seaside Serenity Hill House">
+          Seaside Serenity Hill House
+        </h4>
+        <div className="flex gap-2 items-center mt-[17px]">
+          <LocateIcon className="text-primary text-[20px] text-primary" />
+          <span className="text-base font-normal text-primary font-roboto">
+            4330 Bell Shoals Rd
+          </span>
+        </div>
+        <div className="flex justify-between xl:flex-nowrap gap-3 flex-wrap items-center mt-[25px]">
+          <div className="flex gap-2 items-center w-fit">
+            <Bed className="text-primary text-[20px] text-primary" />
+            <span className="text-base font-normal text-primary font-roboto">
+              4 Beds
+            </span>
+          </div>
+
+          <div className="flex gap-2 items-center w-fit">
+            <Bath className="text-primary text-[20px] text-primary" />
+            <span className="text-base font-normal text-primary font-roboto">
+              7 Baths
+            </span>
+          </div>
+
+          <div className="flex gap-2 items-center w-fit">
+            <SquaresIntersect className="text-primary text-[20px] text-primary" />
+            <span className="text-base font-normal text-primary font-roboto">
+              1200 Sqft
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
