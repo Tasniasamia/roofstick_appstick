606499b0af48fa4778b949c976d97a8e3e69962e
tasniaarhamarham <tasniaarhamarham@gmail.com>
2025-11-07 18:28:13 +0600

    testimonial done

--- components/(site)/home/cardStackSlider.tsx
+++ components/(site)/home/cardStackSlider.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import TestimonialCard from "@/components/common/card/testimonialCard";
 import HeaderTitle from "@/components/common/headerTitle";
+import Image from "next/image";
 
 interface Card {
   id: number;
@@ -56,22 +57,43 @@ export default function CardStackSlider() {
   };
 
   return (
-    <div className=" lg:pt-[120px] w-full bg-red-300 pt-[60px]">
-   
-      <div className="container ">
-      <div className="md:mb-16 sm:mb-8 ">
-      <HeaderTitle
-        title={"What our Client Says About us"}
-        description={
-          "The platform provided a seamless home-buying experience with exceptional professionalism.Their verified listings and expert guidance made the entire process efficient and stress-free."
-        }
-        customClass="items-center text-center  "
-        customClass3="lg:w-[662px] md:w-[427px] w-[80%]"
-        customClass2="md:w-[460px] w-full"
+    <div className=" lg:mt-[120px] w-full relative  mt-[60px]">
+      <Image
+        src="/frame.png"
+        width={500}
+        height={500}
+        alt="frame"
+        className="lg:block hidden h-[200px] w-[200px] object-center absolute top-[261px] left-[335px] "
       />
-      </div>
+         <Image
+        src="/frame.png"
+        width={500}
+        height={500}
+        alt="frame"
+        className="lg:block hidden h-[200px] w-[200px] object-center absolute top-[62px] right-[177px] "
+      />
+      <div className="container ">
+        <div className="md:mb-16 sm:mb-8 ">
+          <HeaderTitle
+            title={"What our Client Says About us"}
+            description={
+              "The platform provided a seamless home-buying experience with exceptional professionalism.Their verified listings and expert guidance made the entire process efficient and stress-free."
+            }
+            customClass="items-center text-center  "
+            customClass3="lg:w-[662px] md:w-[427px] w-[80%]"
+            customClass2="md:w-[460px] w-full"
+          />
+        </div>
         <div className="relative w-full   md:h-[643px] sm:h-[490px] h-[290px] flex sm:items-center sm:justify-center perspective">
           <div className="relative w-full h-full ">
+            <Image
+              src="/testimonial-bg.png"
+              alt="background"
+              width={500}
+              height={500}
+              className="absolute z-10 left-1/2 -translate-x-1/2 top-1/2 lg:block hidden -translate-y-1/2"
+            />
+
             {cards.map((card, index) => {
               const position = getCardPosition(index);
 
