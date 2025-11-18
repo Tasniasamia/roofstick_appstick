2df5aed45544232a281326ed480422a781e1da39
tasniaarhamarham <tasniaarhamarham@gmail.com>
2025-11-18 11:19:26 +0600

    some of doing

---
+++ app/(site)/service/[id]/page.tsx
@@ -0,0 +1,14 @@
+import Banner from '@/components/common/banner';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <Banner routeName='Blog Details'/>
+            
+        </div>
+    );
+};
+
+export default page;
+
---
+++ app/(site)/service/page.tsx
@@ -0,0 +1,15 @@
+import OurService2 from '@/components/(site)/home/ourService2';
+import Banner from '@/components/common/banner';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <Banner routeName='Service'/>
+            <OurService2/>
+            
+        </div>
+    );
+};
+
+export default page;
